c2be545636de023e4e1770b97571a57b562df6a6
Mottelz <shachna@gmail.com>
2017-12-10 18:59:13 -0500

    Adjusted interval to between 30 and 60 minutes.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const bot = require('./src/bot.js')
-var interval = (Math.floor(Math.random() * 150) + 30) * 1000 * 60
+var interval = (Math.floor(Math.random() * 30) + 31) * 1000 * 60
 console.log('The interval is now (ms): '+interval)
 setInterval(bot, interval)
