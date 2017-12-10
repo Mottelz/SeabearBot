8302979623325f72f9e697360a3228ddb5f59586
Mottelz <shachna@gmail.com>
2017-12-10 16:34:40 -0500

    Removed env. Bad practice and needs to be fixed.

--- src/config.js
+++ src/config.js
@@ -1,8 +1,15 @@
-require('dotenv').config()
+//require('dotenv').config()
+
+// module.exports = {
+//     consumer_key: process.env.CONSUMER_KEY,
+//     consumer_secret: process.env.CONSUMER_SECRET,
+//     access_token: process.env.ACCESS_TOKEN,
+//     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+// }
 
 module.exports = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    consumer_key: "2si3SI1LxgcZ5OEM1FtJQl3Zu",
+    consumer_secret: "OJSdRZDb3gzG8sU2SHdudzFKrVaNRRlPUchYyqDhYJpmBg3ltu",
+    access_token: "939951066501861376-fD09446ZTYFzJjjIYQMsBr61QerpNAT",
+    access_token_secret: "WflCFqHnobvqydwtA2HFBQdsRtyTG796d0srL2mVkXzfo",
 }
