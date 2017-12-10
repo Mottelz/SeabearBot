e0b7d9e200cc70cce0486652632cab749f204960
Mottelz <shachna@gmail.com>
2017-12-10 16:02:28 -0500

    Updated interval and removed call.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const bot = require('./src/bot.js')
 
-var interval = (Math.floor(Math.random() * 60) + 1) * 1000
-bot()
+var interval = (Math.floor(Math.random() * 180) + 1) * 1000
+//bot()
 setInterval(bot, interval)
