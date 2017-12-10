2341519c59f6ec4df845f3a37228c39c69757aca
Mottelz <shachna@gmail.com>
2017-12-10 17:11:53 -0500

    Put env back and added procfile

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const bot = require('./src/bot.js')
-
 var interval = (Math.floor(Math.random() * 180) + 1) * 1000
 //bot()
 setInterval(bot, interval)
--- src/config.js
+++ src/config.js
@@ -1,15 +1,8 @@
-//require('dotenv').config()
-
-// module.exports = {
-//     consumer_key: process.env.CONSUMER_KEY,
-//     consumer_secret: process.env.CONSUMER_SECRET,
-//     access_token: process.env.ACCESS_TOKEN,
-//     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-// }
+require('dotenv').config()
 
 module.exports = {
-    consumer_key: "2si3SI1LxgcZ5OEM1FtJQl3Zu",
-    consumer_secret: "OJSdRZDb3gzG8sU2SHdudzFKrVaNRRlPUchYyqDhYJpmBg3ltu",
-    access_token: "939951066501861376-fD09446ZTYFzJjjIYQMsBr61QerpNAT",
-    access_token_secret: "WflCFqHnobvqydwtA2HFBQdsRtyTG796d0srL2mVkXzfo",
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 }
