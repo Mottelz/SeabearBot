b752fd364e45f5bb219d9e9c746f434b47b1a6c3
Mottelz <shachna@gmail.com>
2017-12-10 17:17:27 -0500

    Changed interval to a minimum of 30 minutes and max of 3 hours

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 const bot = require('./src/bot.js')
-var interval = (Math.floor(Math.random() * 180) + 1) * 1000
-//bot()
+var interval = (Math.floor(Math.random() * 150) + 30) * 1000
 setInterval(bot, interval)
