e22bd6e218f352615ba6bb76c9e146bf7a2edb70
Mottelz <mottelz@users.noreply.github.com>
2019-02-08 01:43:43 -0500

    Updated packages to fix vulnerabilities.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const bot = require('./src/bot.js')
-var interval = (Math.floor(Math.random() * 30) + 31) * 1000 * 60
+let interval = (Math.floor(Math.random() * 30) + 31) * 1000 * 60
 console.log('The interval is now (ms): '+interval)
 setInterval(bot, interval)
